7d4ec8b2dd2d14465d90d046f35101d1df0ac408
shubhpatel32 <sdpatel972@gmail.com>
2024-07-05 14:42:04 +0530

    app.jsx changes

--- Fruitnest/src/App.jsx
+++ Fruitnest/src/App.jsx
@@ -1,20 +1,19 @@
-import React, { Suspense } from 'react';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
+import Home from './Pages/Home/Home';
+import Shop from './Pages/Shop/Shop';
+import About from './Pages/About/About';
+import Review from './Pages/Review/Review';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
+import Login from './Pages/Login/Login';
+import Cart from './Pages/Cart/Cart';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { CartProvider } from './Context/CartContext';
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
+import Signup from './Pages/SignUp/Signup';
 
-const Home = React.lazy(() => import('./Pages/Home/Home'));
-const Shop = React.lazy(() => import('./Pages/Shop/Shop'));
-const About = React.lazy(() => import('./Pages/About/About'));
-const Review = React.lazy(() => import('./Pages/Review/Review'));
-const Blog = React.lazy(() => import('./Pages/Blog/Blog'));
-const Contact = React.lazy(() => import('./Pages/Contact/Contact'));
-const Login = React.lazy(() => import('./Pages/Login/Login'));
-const Cart = React.lazy(() => import('./Pages/Cart/Cart'));
-const Signup = React.lazy(() => import('./Pages/SignUp/Signup'));
 
 function App() {
   return (
@@ -23,19 +22,17 @@ function App() {
         <ScrollToTop />
         <CartProvider>
           <Header />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route exact path="/cart" element={<Cart />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/signup" element={<Signup />} />
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/shop" element={<Shop />} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/review" element={<Review />} />
-              <Route exact path="/blog" element={<Blog />} />
-              <Route exact path="/contact" element={<Contact />} />
-            </Routes>
-          </Suspense>
+          <Routes>
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/shop" element={<Shop />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/review" element={<Review />} />
+            <Route exact path="/blog" element={<Blog />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
           <Footer />
         </CartProvider>
       </Router>
